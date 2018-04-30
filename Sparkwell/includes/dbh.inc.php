87e10d9726529971bf46f91fbb9f5179d07be264
rmit-s3547567-Mason-Qian <s3547567@student.rmit.edu.au>
2018-04-30 17:39:13 +1000

    Update dbh.inc.php

--- Sparkwell/includes/dbh.inc.php
+++ Sparkwell/includes/dbh.inc.php
@@ -1,11 +1,13 @@
 <?php
 
-$dbServername ="sparkwell2018.mysql.database.azure.com";
+$dbServername ="sparkwell2018.mysql.database.azure.com:3306";
 $dbUsername = "sparkwelladmin@sparkwell2018";
 $dbPassword = "sparkwellNASM2018";
 $dbName = "loginsystem";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 
