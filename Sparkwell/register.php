1930867c9bb969490a9bd46965eda20ae7d9d943
rmit-s3547567-Mason-Qian <s3547567@student.rmit.edu.au>
2018-05-01 11:53:56 +1000

    Update register.php

--- Sparkwell/register.php
+++ Sparkwell/register.php
@@ -5,15 +5,21 @@
 <section class="main-container">
 	<div class="main-wrapper">	
 		<h2>Register</h2>
-		<form class="register-form" action="includes/register.inc.php" method="POST">
-			<input type="text" name="serial"  placeholder="Serial Number">
-			<input type="text" name="first"  placeholder="Firstname">
-			<input type="text" name="last"  placeholder="Lastname">
-			<input type="text" name="email"  placeholder="E-mail">
-			<input type="text" name="uname"  placeholder="Username">
-			<input type="password" name="pword"  placeholder="Password">
-			<button type="submit" name="submit">Register</button>
-		</form>
+		<?php
+			if (isset($_GET['register']) == "success") {
+				echo 'Thank you for registering!';
+			} else {
+				echo '<form class="register-form" action="includes/register.inc.php" method="POST">
+				<input type="text" name="serial"  placeholder="Serial Number">
+				<input type="text" name="first"  placeholder="Firstname">
+				<input type="text" name="last"  placeholder="Lastname">
+				<input type="text" name="email"  placeholder="E-mail">
+				<input type="text" name="uname"  placeholder="Username">
+				<input type="password" name="pword"  placeholder="Password">
+				<button type="submit" name="submit">Register</button>
+				</form>';
+			}
+		?>
 	</div>
 </section>
 
